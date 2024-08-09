a23ea12b707de8c12f8d9d4f341e63f43995b6de
hmg <1805941581@qq.com>
2024-08-09 16:38:51 +0800

    fix: 取消 commit message 长度限制

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,6 @@
 module.exports = {
-  extends: ["@commitlint/config-conventional"]
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "body-max-line-length": [0, "always", 0] // 禁用 body-max-line-length 规则
+  }
 }
